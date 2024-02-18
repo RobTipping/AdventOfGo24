14f04b0240ca0926154f66aae187e3870a492e18
Rob Tipping <61379257+RobTipping@users.noreply.github.com>
2024-02-18 20:41:34 +0000

    learning to split strings

--- dayTwo/dayTwo.go
+++ dayTwo/dayTwo.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+var redMax = 12
+var greenMax = 13
+var blueMax = 14
+
+var testString = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+
 
 func main() {
     fmt.Println("test world")
+    //tempChar:= testString[5]
+    fmt.Println(strings.Split(testString, ":"))
+    fmt.Println(strings.SplitAfter(testString, ":"))
+    firstSplit := strings.Split(testString, ": ")
+    fmt.Println(firstSplit[0])
+    secondSplit := strings.Split(firstSplit[1], "; ")
+    for _, x := range secondSplit {
+        fmt.Println(x)
+        for _, y := range strings.Split(x, ", ") {
+            fmt.Println(y)
+        }
+    }
 }
