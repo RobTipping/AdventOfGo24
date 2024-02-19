04f346481f4dc0cccf31ed7277fa232b5fe32345
Rob Tipping <61379257+RobTipping@users.noreply.github.com>
2024-02-19 20:53:57 +0000

    split string and got number

--- dayTwo/dayTwo.go
+++ dayTwo/dayTwo.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 var redMax = 12
 var greenMax = 13
 var blueMax = 14
+
+var red = 0
+var green = 0
+var blue = 0
 
 var testString = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
 
@@ -24,6 +29,19 @@ func main() {
         fmt.Println(x)
         for _, y := range strings.Split(x, ", ") {
             fmt.Println(y)
+            if strings.Contains(y, "red") == true {
+                temp := strings.TrimSuffix(y," red")
+                r, err := strconv.Atoi(temp)
+                if err == nil {
+                    red = r
+                }
+                fmt.Println(red+1000)
+            }
         }
+    }
+    s := "123"
+    q, err := strconv.Atoi(s)
+    if err == nil {
+        fmt.Println(q+5)
     }
 }
